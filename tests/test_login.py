01a62511b7a382bf3e52ab089d4d28ad8db7d7f7
Rahul Ranjan <rahul.sinha.886289@gmail.com>
2026-05-25 22:29:41 +0530

    Fixed header issue

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright, expect
 from datetime import datetime
 import pytest
 def test_create_project(playwright:Playwright):
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://admin-test.granitestack.ai/admin/login")
